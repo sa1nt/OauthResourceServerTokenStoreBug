1ef86f75866aabe0eed6d2825b9d5e27112ff592
Anatoly Mozgov <anatoly.mozgov@cte.lv>
2016-04-11 19:15:28 +0300

    first commit

---
+++ src/main/java/lv/sa1nt/test/AppConfig.java
@@ -0,0 +1,72 @@
+package lv.sa1nt.test;
+
+import java.io.IOException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
+import redis.clients.jedis.JedisPoolConfig;
+import redis.embedded.RedisServer;
+
+@Configuration
+@EnableResourceServer
+@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+public class AppConfig extends ResourceServerConfigurerAdapter {
+    
+    static Logger logger = LoggerFactory.getLogger(AppConfig.class);
+    
+    @Bean
+    public JedisConnectionFactory connectionFactory() throws IOException { 
+        RedisServer redisServer = new RedisServer();
+        redisServer.start();
+        
+        logger.info("Embedded Redis server started");
+//        System.out.println("Embedded Redis server started");
+        
+        JedisPoolConfig poolConfig = new JedisPoolConfig();
+        JedisConnectionFactory factory = new JedisConnectionFactory(poolConfig);
+        return factory;
+    }
+    
+    @Bean
+    public TokenStore tokenStore() throws IOException { 
+        return new RedisTokenStore(connectionFactory());
+    }
+    
+//    @Bean
+    public ResourceServerTokenServices tokenServices() throws IOException { 
+        DefaultTokenServices services = new DefaultTokenServices();
+        services.setTokenStore(tokenStore());
+        return services;
+    }
+    
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+        resources.tokenStore(tokenStore());
+        resources.tokenServices(tokenServices());
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                .antMatcher("/**")
+                .authorizeRequests()
+                .antMatchers("/help**", "/test**")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and().logout().logoutSuccessUrl("/help").permitAll()
+                ;
+    }
+}
